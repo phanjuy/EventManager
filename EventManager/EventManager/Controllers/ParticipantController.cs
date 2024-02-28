1f22d11b5f24b620f03ff7942e50a22d672aa295
Duy Phan <phanjuy@hotmail.com>
2024-02-28 10:10:53 +0100

    Add ParticipantController

---
+++ EventManager/EventManager/Controllers/ParticipantController.cs
@@ -0,0 +1,43 @@
+﻿using EventManager.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace EventManager.Controllers
+{
+    [Route("api/participants")]
+    [ApiController]
+    public class ParticipantController : ControllerBase
+    {
+        // GET: api/<ValuesController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        [HttpGet("{id}/events")]
+        public string GetEventsForParticipant(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<ValuesController>
+        [HttpPost]
+        public void CreateParticipant([FromBody] ParticipantDto participant)
+        {
+        }
+
+        // PUT api/<ValuesController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<ValuesController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
