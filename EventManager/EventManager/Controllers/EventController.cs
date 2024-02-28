f45fdf441f4d8f6057aada765466ef6e1170ee99
Duy Phan <phanjuy@hotmail.com>
2024-02-28 10:10:38 +0100

    Add EventController

---
+++ EventManager/EventManager/Controllers/EventController.cs
@@ -0,0 +1,38 @@
+﻿using EventManager.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EventManager.Controllers
+{
+    [Route("api/events")]
+    [ApiController]
+    public class EventController : ControllerBase
+    {
+        public EventController()
+        {
+
+        }
+
+        [HttpGet("{id}")]
+        public string GetEvent(int id)
+        {
+            return "value";
+        }
+
+        [HttpPost]
+        public void Post([FromBody] EventDto eventDto)
+        {
+        }
+
+        // PUT api/<EventController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<EventController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
